93f3e53d3a5264893ab22e3d89fd62ef77770684
Louisse Amadeo Romero <111945837+Amadeo0312@users.noreply.github.com>
2024-03-30 13:40:35 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,91 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import numpy as np
+import cv2
+from tqdm import tqdm
+import tensorflow as tf
+from patchify import patchify
+from train import load_dataset, create_dir
+from metrics import dice_loss, dice_coef
+
+
+""" UNETR  Configuration """
+cf = {
+    "image_size": 256,
+    "num_channels": 3,
+    "num_layers": 12,
+    "hidden_dim": 128,
+    "mlp_dim": 32,
+    "num_heads": 6,
+    "dropout_rate": 0.1,
+    "patch_size": 16
+}
+cf["num_patches"] = (cf["image_size"]**2) // (cf["patch_size"]**2)
+cf["flat_patches_shape"] = (
+    cf["num_patches"],
+    cf["patch_size"] * cf["patch_size"] * cf["num_channels"]
+)
+
+
+if __name__ == "__main__":
+    """ Seeding """
+    np.random.seed(42)
+    tf.random.set_seed(42)
+
+    """ Directory for storing files """
+    results_dir = os.path.join(os.path.dirname(__file__), "results")
+    create_dir(results_dir)
+    print("Results directory:", results_dir)
+
+    """ Load the model """
+    model_path = os.path.join("files", "model.keras")
+    model = tf.keras.models.load_model(model_path, custom_objects={"dice_loss": dice_loss, "dice_coef": dice_coef})
+
+    """ Dataset """
+    dataset_path = "mandible-segmentation"
+    (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_dataset(dataset_path)
+
+    print(f"Train: \t{len(train_x)} - {len(train_y)}")
+    print(f"Valid: \t{len(valid_x)} - {len(valid_y)}")
+    print(f"Test: \t{len(test_x)} - {len(test_y)}")
+
+    """ Prediction """
+    for x, y in tqdm(zip(test_x, test_y), total=len(test_x)):
+        """ Extracting the name """
+        name = os.path.basename(x)
+        print("Processing:", name)
+
+        """ Reading the image """
+        image = cv2.imread(x, cv2.IMREAD_COLOR)
+        image = cv2.resize(image, (cf["image_size"], cf["image_size"]))
+        x = image / 255.0
+
+        patch_shape = (cf["patch_size"], cf["patch_size"], cf["num_channels"])
+        patches = patchify(x, patch_shape, cf["patch_size"])
+        patches = np.reshape(patches, cf["flat_patches_shape"])
+        patches = patches.astype(np.float32)
+        patches = np.expand_dims(patches, axis=0)
+
+        """ Read Mask """
+        mask = cv2.imread(y, cv2.IMREAD_GRAYSCALE)
+        mask = cv2.resize(mask, (cf["image_size"], cf["image_size"]))
+        mask = mask / 255.0
+        mask = np.expand_dims(mask, axis=-1)
+        mask = np.concatenate([mask, mask, mask], axis=-1)
+
+        """ Prediction """
+        pred = model.predict(patches, verbose=0)[0]
+        pred = np.concatenate([pred, pred, pred], axis=-1)
+
+        """ Save final mask """
+        line = np.ones((cf["image_size"], 10, 3)) * 255
+        cat_images = np.concatenate([image, line, mask*255, line, pred*255], axis=1)
+        save_image_path = os.path.join(results_dir, name)
+        try:
+            cv2.imwrite(save_image_path, cat_images)
+            print("Saved:", save_image_path)
+        except Exception as e:
+            print("Error occurred while saving:", e)
+
+print("Processing complete.")
